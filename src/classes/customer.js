0778899f78630ab9108e1e93e929f00b3a4c25f8
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-03 15:14:52 -0800

    feature: find customer bookings

--- src/classes/customer.js
+++ src/classes/customer.js
@@ -2,7 +2,27 @@ class Customer {
     constructor(customerObj) {
         this.id = customerObj.id
         this.name = customerObj.name
+        this.spent = 0
+        this.pastBookings = []
+        this.upcomingBookings = []
+        this.allBookings = []
     }
+    findCustomerBookings(bookingData) {
+        this.allBookings = bookingData.filter(currBooking => {
+            return currBooking.userID === this.id
+        })
+    }
+    totalCost() {
+        this.spent = this.allBookings.reduce((total, booking) => {
+            total += this.allBookings
+   
+    })
 }
+
+
+}
+
+
+
 
 export default Customer
