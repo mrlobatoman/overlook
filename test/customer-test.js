118c4cb62c12e628a12663038052278ff9efeaf2
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-04 15:31:03 -0800

    Add: testing to pass current date, past and upcoming bookings

--- test/customer-test.js
+++ test/customer-test.js
@@ -1,14 +1,24 @@
-import { customers } from './test-data'
+import { customers, bookings, rooms } from './test-data'
 import { expect } from 'chai'
 import Customer from '../src/classes/customer'
+
+let currentDate
 
 describe('Customer', () => {
     let customer1
     let customer2
+    
 
     beforeEach(() => {
         customer1 = new Customer(customers[0])
         customer2 = new Customer(customers[1])
+        
+        const date = new Date()
+        let currentDay = date.getDate()
+        let currentMonth = date.getMonth()
+        let currentYear = date.getFullYear()
+        let currentDate = `${currentDay}${currentMonth}${currentYear}`
+        return currentDate = Number(currentDate)
     }) 
 
     it('should be a function', function() {
@@ -18,7 +28,6 @@ describe('Customer', () => {
     it('should create a new instance of customer', function() {
         expect(customer1).to.be.an.instanceOf(Customer)
         expect(customer2).to.be.an.instanceOf(Customer)
-        console.log(customer1)
     })
 
     it('should have an id value', function() {
@@ -29,6 +38,11 @@ describe('Customer', () => {
     it('should have name of customer', function() {
         expect(customer1.name).to.equal('Leatha Ullrich')
         expect(customer2.name).to.equal('Rocio Schuster')
+    })
+
+    it('should start with 0 spent', function() {
+        expect(customer1.spent).to.equal(0)
+        expect(customer2.spent).to.equal(0)
     })
 
     it('should be able to store past bookings', function() {
@@ -46,5 +60,47 @@ describe('Customer', () => {
         expect(customer2.allBookings).to.deep.equal([])
     })
 
+    it('should be able to find customer bookings', function() {
+      customer1.findCustomerBookings(bookings)
+      expect(customer1.allBookings).to.deep.equal([bookings[0]])
+     
+      customer2.findCustomerBookings(bookings)
+      expect(customer2.allBookings).to.deep.equal([bookings[2], bookings[3]])
+    })
 
-})
+    it('should add total spent on rooms', function() {
+        customer1.findCustomerBookings(bookings)
+        customer1.totalCost(rooms)
+        expect(customer1.spent).to.equal(358.4)
+
+        // customer2.findCustomerBookings(bookings)
+        // customer2.totalCost(rooms)
+        // expect(customer2.spent).to.equal(477.38)
+    })
+
+    it('should get todays date in number format', function() {
+        let result1 = customer1.getCurrentDate()
+        expect(result1).to.equal(result1)
+    })
+
+    it('should get upcoming bookings', function() {
+        customer1.getUpcomingBookings(bookings)
+        expect(customer1.upcomingBookings).to.deep.equal([{
+            "id": "5fwrgu4i7k55hl6t8",
+            "userID": 1,
+            "date": "2024/02/05",
+            "roomNumber": 12
+        }
+        ])
+    })
+    it('should get past bookings', function() {
+        customer2.getPastBookings(bookings)
+        expect(customer2.pastBookings).to.deep.equal([{
+            "id": "5fwrgu4i7k55hl6wx",
+            "userID": 2,
+            "date": "2022/01/17",
+            "roomNumber": 17
+        }
+        ])
+    })
+ })
