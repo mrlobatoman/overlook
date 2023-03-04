e34b02fdc15419fb94124a0640c35c1d27b4dc8d
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-04 09:48:14 -0800

    Adds: testing to check all object properties are functioning properly

--- test/customer-test.js
+++ test/customer-test.js
@@ -14,4 +14,37 @@ describe('Customer', () => {
     it('should be a function', function() {
         expect(Customer).to.be.a('function')
     })
+
+    it('should create a new instance of customer', function() {
+        expect(customer1).to.be.an.instanceOf(Customer)
+        expect(customer2).to.be.an.instanceOf(Customer)
+        console.log(customer1)
+    })
+
+    it('should have an id value', function() {
+        expect(customer1.id).to.equal(1)
+        expect(customer2.id).to.equal(2)
+    })
+
+    it('should have name of customer', function() {
+        expect(customer1.name).to.equal('Leatha Ullrich')
+        expect(customer2.name).to.equal('Rocio Schuster')
+    })
+
+    it('should be able to store past bookings', function() {
+        expect(customer1.pastBookings).to.deep.equal([])
+        expect(customer2.pastBookings).to.deep.equal([])
+    })
+
+    it('should be able to store upcoming bookings', function() {
+        expect(customer1.upcomingBookings).to.deep.equal([])
+        expect(customer2.upcomingBookings).to.deep.equal([])
+    })
+
+    it('should be able to store all bookings', function() {
+        expect(customer1.allBookings).to.deep.equal([])
+        expect(customer2.allBookings).to.deep.equal([])
+    })
+
+
 })
