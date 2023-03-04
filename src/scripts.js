423ec7f444b684be96fabe8dd15fa02e883ffd2e
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-03 17:20:36 -0800

    Add: fetching for all apis, assigns all data to a var, and assigns data to new class instance

--- src/scripts.js
+++ src/scripts.js
@@ -6,9 +6,51 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/booking-logo.png'
+import Customer from './classes/customer.js'
+import Booking from './classes/booking.js'
+import Room from './classes/room.js'
+
+let bookingData 
+let customerData = 0
+let roomData
 
 
-fetch('http://localhost:3001/api/v1/customers')
- .then(resp => resp.json())
- .then(data => console.log(data))
+let allMotelData
+
+const customersDataF = fetch('http://localhost:3001/api/v1/customers')
+    .then(resp => resp.json())
+
+ const roomsDataF = fetch('http://localhost:3001/api/v1/rooms')
+    .then(resp => resp.json())
+
+ const bookingsDataF = fetch('http://localhost:3001/api/v1/bookings')
+    .then(resp => resp.json())
+
+ Promise.all([customersDataF, roomsDataF, bookingsDataF])
+    .then((data) => {
+        allMotelData = {
+            customers: data[0].customers,
+            rooms: data[1].rooms,
+            bookings: data[2].bookings
+        }
+        console.log(allMotelData)
+        return allMotelData
+    })
+    .then(
+        (allMotelData) => {
+            customerData = new Customer(allMotelData.customers[0])
+            bookingData = new Booking(allMotelData.bookings)
+            roomData = new Room(allMotelData.rooms)
+            console.log(customerData)
+            console.log(bookingData)
+            console.log()
+        }
+    )
+
+    
+
+
+
+
+
 console.log('This is the JavaScript entry file - your code begins here.');
