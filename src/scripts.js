12b19d867f62eb88384f9a3e74c71eb9894f834d
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-05 12:55:54 -0800

    Add: strings data upon login

--- src/scripts.js
+++ src/scripts.js
@@ -15,6 +15,26 @@ import Room from './classes/room.js'
 let bookingData 
 let customerData = 0
 let roomsData
+let customer
+let clientNumber
+
+
+let customerURL
+const bookingsURL = 'http://localhost:3001/api/v1/bookings'
+const roomsURL = 'http://localhost:3001/api/v1/rooms'
+
+const loginForm = document.querySelector('#loginForm')
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const loginButton = document.querySelector('#loginButton')
+const incorrectLogin = document.querySelector('#incorrectLoginText')
+const loginView = document.querySelector('#loginView')
+const currentUser = document.querySelector('#userText')
+
+loginButton.addEventListener('click', verifyLogin)
+loginForm.addEventListener('click', preventLoad)
+
+
 
 
 let allMotelData
@@ -33,50 +53,71 @@ console.log(bookingsDataF)
 
 
 
- Promise.all([customersDataF, roomsDataF, bookingsDataF])
-    .then((data) => {
-        allMotelData = {
-            customers: data[0].customers,
-            rooms: data[1].rooms,
-            bookings: data[2].bookings
-        }
-        return allMotelData
-    })
-    .then(
-        (allMotelData) => {
-            customerData = new Customer(allMotelData.customers[0])
-            bookingData = allMotelData.bookings.map(booking => new Booking(booking))
-            roomsData = allMotelData.rooms.map(room => new Room(room))
-            console.log(customerData)
-            console.log(bookingData)
-            console.log(roomsData)
-        }
-    )
 
-    function loadCustomer(data) {
-        currentCustomer = new Customer(data)
-        allBookings = currentCustomer.createBooking(allMotelData.bookings)
-        return currentCustomer
+   //========================Functions=========================
+
+   function verifyLogin(){
+    let usernameInput = username.value
+    let passwordInput = password.value
+    let userID
+    let userName
+
+    let getID = usernameInput.split('')
+    if(getID.length === 9 || getID.length === 10) {
+        userID = getID.splice(8,2).join('')
+        userName = getID.splice(0,8).join('')
+        console.log(userID)
+        console.log(userName)
+
+    if(userName === 'customer' && (userID > 0 && userID <=50)) {
+        clientNumber = (Number(userID)).toString()
+        customerURL = `http://localhost:3001/api/v1/customers/${clientNumber}`
+        }
+    } else {
+        incorrectLogin.classList.remove('hidden')
     }
+}
 
-    function displayUserInfo() {
+Promise.all([customersDataF, roomsDataF, bookingsDataF])
+.then((data) => {
+    allMotelData = {
+        customers: data[0].customers,
+        rooms: data[1].rooms,
+        bookings: data[2].bookings
+    }
+    return allMotelData
+})
+.then(
+    (allMotelData) => {
+        customerData = allMotelData.customers.map(customer => new Customer(customer))
+        bookingData = allMotelData.bookings.map(booking => new Booking(booking))
+        roomsData = allMotelData.rooms.map(room => new Room(room))
+        console.log(customerData)
+        console.log(bookingData)
+        console.log(roomsData)
+    }
+)
+    
+    
+        
+ 
+
+    function displayAccountInfo() {
+        currentUser.innerText = customer.name + "'s account"
         gatherAccountInfo()
-    }
-
-    function loadRooms(data) {
-        allRooms = data.map(currentBooking => new Room(currentBooking))
-    }
+    } 
 
     function gatherAccountInfo() {
-        currentCustomer.getUpcomingBookings(allBookings)
-        userUpcomingBookings = currentCustomer.upcomingBookings
-        currentCustomer.getPastBookings(allBookings)
-        userPastBookings = currentCustomer.pastBookings
-       
+        customer.getUpcomingBookings(allBookings)
+        userUpcomingBookings = customer.upcomingBookings
+        customer.getPastBookings(allBookings)
+        userPastBookings = customer.pastBookings
     } 
 
 
 
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+function preventLoad(event) {
+    event.preventDefault()
+}
